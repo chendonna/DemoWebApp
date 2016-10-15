859fa6b37cbd9692b077f0a89ef2d21fa0042b9a
chendonna <chendonna@gmail.com>
2016-10-15 13:46:48 -0400

    added weather api

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,30 @@
-
+$(document).ready(function() {
+        getWeather();
+});
 function myFunction() {
   document.getElementById("demo").innerHTML = "NEWWW PARAGRAPH #javascript #swag";
 }
 
+function searchWeather() {
+        var searchQuery = $('.search').val();
+        getWeather(searchQuery);
+}
+
+function getWeather() {
+        var url = 'http://api.openweathermap.org/data/2.5/weather?';
+        var params = {
+                units = 'imperial',
+                APPID = process.env.API_KEY,
+        }
+        if (searchQuery) {
+                params.q = searchQuery;
+        } else {
+                params.id = 4930956;
+        }
+        $.ajax(url + $param(params), {
+                sucess:function(data) {
+                        $('.city').text(data.name);
+                        $('.temp').text(data.main.temp + ' F');
+                }
+        });
+}
